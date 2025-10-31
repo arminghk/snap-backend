8573e3bef8175ef239a8c948cd3d4adfdaa11b1f
Arminghk <armin.ghk1371@gmail.com>
2025-10-31 16:07:36 +0330

    setup Router Module and Namespace for configs

---
+++ apps/Gateway/src/rest/passenger/passenger.module.ts
@@ -0,0 +1,10 @@
+
+import { Module } from '@nestjs/common';
+import { PassengerAuthController } from './auth/auth.controller';
+import { PassengerAuthService } from './auth/auth.service';
+
+@Module({
+    controllers:[PassengerAuthController],
+    providers:[PassengerAuthService]
+})
+export class PassengerModule {}
