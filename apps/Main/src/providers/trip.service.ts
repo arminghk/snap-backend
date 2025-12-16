d30a26aeca6a81fe2c9b1b4eba917792c06bdcac
Arminghk <armin.ghk1371@gmail.com>
2025-12-16 17:36:17 +0330

    driver accept trip

--- apps/Main/src/providers/trip.service.ts
+++ apps/Main/src/providers/trip.service.ts
@@ -1,7 +1,7 @@
 import { HttpStatus, Injectable } from '@nestjs/common';
 import { PostgresService } from 'src/databases/postgres/postgres.service';
 import { ServiceClientContextDto, ServiceResponseData, SrvError } from 'src/services/dto';
-
+import { Transaction } from 'sequelize';
 
 @Injectable()
 export class TripService {
@@ -34,5 +34,47 @@ export class TripService {
       message: 'Trip created successfully',
       data: trip,
     };
+  }
+
+  async accept({ query }: ServiceClientContextDto): Promise<ServiceResponseData> {
+    const { tripId, driverId } = query;
+
+    if (!tripId || !driverId) {
+      throw new SrvError(HttpStatus.BAD_REQUEST, 'Invalid input');
+    }
+
+    return await this.pg.connection.transaction(
+      async (transaction: Transaction) => {
+        const trip = await this.pg.models.Trip.findOne({
+          where: {
+            id: tripId,
+            status: "REQUESTED",
+          },
+          lock: transaction.LOCK.UPDATE,
+          transaction,
+        });
+
+        if (!trip) {
+          throw new SrvError(
+            HttpStatus.CONFLICT,
+            'Trip already accepted or not found',
+          );
+        }
+
+        await trip.update(
+          {
+            driverId,
+            status: "ACCEPTED",
+            acceptedAt: new Date(),
+          },
+          { transaction },
+        );
+
+        return {
+          message: 'Trip accepted successfully',
+          data: trip,
+        };
+      },
+    );
   }
 }
